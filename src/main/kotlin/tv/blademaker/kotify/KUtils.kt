1f148afa8211541b7c5f5e58844a7e7cec3a5ad0
Blad3Mak3r <blademaker.live@gmail.com>
2022-10-26 20:25:17 +0200

    Fix this

--- src/main/kotlin/tv/blademaker/kotify/KUtils.kt
+++ src/main/kotlin/tv/blademaker/kotify/KUtils.kt
@@ -4,5 +4,5 @@ import java.net.URLEncoder
 import java.nio.charset.Charset
 
 internal fun Iterable<Kotify.Scope>.parse(): String {
-    return URLEncoder.encode(joinToString("+") { it.value }, Charset.defaultCharset())
+    return joinToString("+") { it.value }
 }
